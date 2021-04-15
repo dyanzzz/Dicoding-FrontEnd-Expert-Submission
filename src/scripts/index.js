ed779b71a686a0c1348a75e4a78ee4c3e93b6c68
Dian Tri Budianto <dian.budianto@fastratabuana.co.id>
2021-04-15 08:39:36 +0700

    fix update review code from reviewer dicoding

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 import '../styles/responsive.css';
-const data = require("../DATA.json");
+import data from '../DATA.json';
 
 console.log('Hello Coders! :)');
 
